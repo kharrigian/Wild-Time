0bd2c36e0431604e9d54ea191328e5e536fb820a
Keith Harrigian <keithharrigian@gmail.com>
2022-10-20 12:20:31 -0400

    Fix Google Drive links

--- download_datasets.py
+++ download_datasets.py
@@ -19,7 +19,7 @@ def download_gdrive(url, save_path, is_folder):
 
 def download_arxiv(data_dir):
     download_gdrive(
-        url='https://drive.google.com/file/d/17JdUhLWMSm75Tw6z7JBegjz_eWNW4Bnz/view?usp=sharing',
+        url='https://drive.google.com/uc?id=17JdUhLWMSm75Tw6z7JBegjz_eWNW4Bnz',
         save_path=os.path.join(data_dir, 'arxiv.pkl'),
         is_folder=False
     )
@@ -27,12 +27,12 @@ def download_arxiv(data_dir):
 
 def download_drug(data_dir):
     download_gdrive(
-        url='https://drive.google.com/drive/folders/1796kUMCTs8r0dnQjBiLt7YTm6ZtZAd3f?usp=sharing',
+        url='https://drive.google.com/drive/folders/1796kUMCTs8r0dnQjBiLt7YTm6ZtZAd3f',
         save_path=os.path.join(data_dir, 'Drug-BA'),
         is_folder=True
     )
     download_gdrive(
-        url='https://drive.google.com/file/d/179g3KTOG2mBTZzF6lxG6iodNp8xmdxpg/view?usp=sharing',
+        url='https://drive.google.com/uc?id=179g3KTOG2mBTZzF6lxG6iodNp8xmdxpg',
         save_path=os.path.join(data_dir, 'drug_preprocessed.pkl'),
         is_folder=False
     )
@@ -40,7 +40,7 @@ def download_drug(data_dir):
 
 def download_fmow(data_dir):
     download_gdrive(
-        url='https://drive.google.com/file/d/17Jplcs6QSCZb5pmE8xDKKwQZnrjBClsf/view?usp=sharing',
+        url='https://drive.google.com/uc?id=17Jplcs6QSCZb5pmE8xDKKwQZnrjBClsf',
         save_path=os.path.join(data_dir, 'fmow.pkl'),
         is_folder=False
     )
@@ -48,23 +48,23 @@ def download_fmow(data_dir):
 
 def download_huffpost(data_dir):
     download_gdrive(
-        url='https://drive.google.com/file/d/17Jp9CiEG69zOjAnqCK0sHu8SeWQ76uPd/view?usp=sharing',
+        url='https://drive.google.com/uc?id=17Jp9CiEG69zOjAnqCK0sHu8SeWQ76uPd',
         save_path=os.path.join(data_dir, 'huffpost.pkl'),
         is_folder=False
     )
 
 
 def download_mimic(args, data_dir):
-    if not os.path.exists('./Data/mimic_stay_dict.pkl'):
+    if not os.path.exists('./raw-data/mimic_stay_dict.pkl'):
         get_stay_dict(data_dir)
-    data = pickle.load(open('./Data/mimic_stay_dict.pkl', 'rb'))
+    data = pickle.load(open('./raw-data/mimic_stay_dict.pkl', 'rb'))
     preprocess_MIMIC(data, 'readmission', args)
     preprocess_MIMIC(data, 'mortality', args)
 
 
 def download_precipitation(data_dir):
     download_gdrive(
-        url='https://drive.google.com/file/d/19BnZT3VxYEtNIEj0fN76UcGG2GP2JP65/view?usp=sharing',
+        url='https://drive.google.com/uc?id=19BnZT3VxYEtNIEj0fN76UcGG2GP2JP65',
         save_path=os.path.join(data_dir, 'weather.pkl'),
         is_folder=False
     )
@@ -72,7 +72,7 @@ def download_precipitation(data_dir):
 
 def download_yearbook(data_dir):
     download_gdrive(
-        url='https://drive.google.com/file/d/17I50QFmMAowDRswOD0sr-uq9tQZdjM4v/view?usp=sharing',
+        url='https://drive.google.com/uc?id=17I50QFmMAowDRswOD0sr-uq9tQZdjM4v',
         save_path=os.path.join(data_dir, 'yearbook.pkl'),
         is_folder=False
     )
@@ -100,7 +100,7 @@ def download_datasets(
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download datasets')
-    parser.add_argument('--data_dir', type=str, default='./Data')
+    parser.add_argument('--data_dir', type=str, default='./raw-data/')
     parser.add_argument('--datasets', nargs='*', type=str,
                         default=['arxiv', 'drug', 'fmow', 'huffpost', 'mimic', 'precipitation', 'yearbook'])
     args = parser.parse_args()
